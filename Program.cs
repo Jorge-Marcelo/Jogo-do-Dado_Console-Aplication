d2c9ecc552873938157530b96e49a31e54fd8730
Jorge-Marcelo <jorge3258@hotmail.com>
2024-04-26 17:33:46 -0300

    Upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo_do_Dado
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			JogoDado();
+		}
+
+
+		static void JogoDado()
+		{
+			int NumeroAleatorioJogaor;
+			int NumeroAleatorioCPU;
+
+			int PontosJogador = 0;
+			int PontosCPU = 0;
+
+			Random random = new Random();
+
+			for (int contador = 0; contador < 10; contador++)
+			{
+				Console.WriteLine("Digite qualquer tecla para rodar o dado");
+				Console.ReadKey();/*Captuar qualquer tecla do teclado*/
+
+
+				/*Gerar um numero aleatorio entre 1 e 6*/
+				NumeroAleatorioJogaor = random.Next(1, 7);
+				Console.WriteLine($"Voce rolou o dado {NumeroAleatorioJogaor} vezes");
+
+				Console.WriteLine("......");
+				Thread.Sleep(3000);
+
+				NumeroAleatorioCPU = random.Next(1, 7);
+				Console.WriteLine($"A CPU rolou o dado {NumeroAleatorioCPU} ");
+
+				Console.WriteLine("......");
+				Thread.Sleep(3000);
+
+				/*Se o jogador rolou o dado mais vezes que a CPU*/
+				if (NumeroAleatorioJogaor > NumeroAleatorioCPU)
+				{
+					PontosJogador++;
+					Console.WriteLine("O jogador venceu esse round");
+					Console.WriteLine("--------------------------------------");
+
+				}
+				else if (NumeroAleatorioJogaor < NumeroAleatorioCPU)
+				{
+					PontosCPU++;
+					Console.WriteLine("A CPU venceu esse round");
+					Console.WriteLine("--------------------------------------");
+
+				}
+				else
+				{
+					Console.WriteLine("EMPATOU NESSA RODADA!");
+					Console.WriteLine("--------------------------------------");
+
+				}
+			}
+
+			if (PontosJogador > PontosCPU) { Console.WriteLine("O jogador VENCEU!"); }
+			else if (PontosJogador < PontosCPU) { Console.WriteLine("A CPU VENCEU!"); }
+			else { Console.WriteLine("EMPATOU"); }
+
+		}
+
+	}
+}
